4bb9eef1c080dbc13d69296204734a532b9f2eeb
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2025-07-10 21:52:44 +0530

    initial commit

---
+++ LearnNavBotSheet/app/src/main/java/com/example/learnnavbotsheet/Screens.kt
@@ -0,0 +1,10 @@
+package com.example.learnnavbotsheet
+
+sealed class Screens(val screen: String) {
+    data object Home: Screens("home")
+    data object Profile: Screens("profile")
+    data object Settings: Screens("settings")
+    data object Search: Screens("search")
+    data object Notifications: Screens("notifications")
+    data object Post: Screens("post")
+}
